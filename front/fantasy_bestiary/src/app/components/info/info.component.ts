a99d31a7da58b3f4aed8184f259f5f0d3db4dca5
JakovM <jm52768@fer.hr>
2024-01-13 19:50:46 +0100

    new project idea

--- front/fantasy_bestiary/src/app/components/info/info.component.ts
+++ front/fantasy_bestiary/src/app/components/info/info.component.ts
@@ -1,5 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AuthModule, AuthService } from '@auth0/auth0-angular';
+
+bootstrapApplication(AuthModule, {
+  providers: [
+    importProvidersFrom(
+      AuthModule.forRoot({
+        domain: 'dev-vvw64vche480sgak.eu.auth0.com',
+        clientId: 'dTge1bT76Ca92ls1oSDT0woP5fBt5t2p',
+      })
+    ),
+  ],
+});
 
 @Component({
   selector: 'app-info',
@@ -9,5 +22,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './info.component.css'
 })
 export class InfoComponent {
+
+  constructor(public _auth:AuthService) {
+
+  }
 
 }
